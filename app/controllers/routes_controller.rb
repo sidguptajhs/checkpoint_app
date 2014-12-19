692331e9c5362d56eb31369bd3936a80b91afc6e
Rishabh Rao <rishabhsrao@gmail.com>
2014-12-19 19:56:25 +0530

    Add fake routes.

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -6,109 +6,37 @@ class RoutesController < ApplicationController
  "stations": [{
    "name": "London",
    "nlc": "100",
-   "isArrived": true,
+   "isArrived": false,
    "coordinates": {
-     "latitude": "51.5073510",
-     "longitude": "-0.1277580"
+     "latitude": "12.9287304",
+     "longitude": "77.6289364"
    }
  },
  {
-   "name": "London Euston Rail Station",
+   "name": "Old Oak",
    "nlc": "20000",
-   "isArrived": true,
+   "isArrived": false,
    "coordinates": {
-     "latitude": "51.5290340",
-     "longitude": "-0.1346760"
+     "latitude": "12.928588",
+     "longitude": "77.628955"
    }
  },
  {
-   "name": "Watford Junction Rail Station",
+   "name": "Birmingham",
    "nlc": "300",
    "isArrived": false,
    "coordinates": {
-     "latitude": "51.6637730",
-     "longitude": "-0.3968900"
+     "latitude": "12.92845783",
+     "longitude": "77.62893352"
    }
  },
  {
-   "name": "Coventry Rail Station",
+   "name": "Manchester",
    "nlc": "400",
-   "isArrived": true,
-   "coordinates": {
-     "latitude": "52.4011300",
-     "longitude": "-1.5135670"
-   }
- },
- {
-   "name": "Birmingham New Street Rail Station",
-   "nlc": "500",
    "isArrived": false,
    "coordinates": {
-     "latitude": "52.4083670",
-     "longitude": "-1.9635510"
-   }
- },
- {
-   "name": "Sandwell & Dudley Rail Station",
-   "nlc": "600",
-   "isArrived": false,
-   "coordinates": {
-     "latitude": "52.5086700",
-     "longitude": "-2.0115900"
-   }
- },
- {
-   "name": "Wolverhampton Rail Station",
-   "nlc": "700",
-   "isArrived": false,
-   "coordinates": {
-     "latitude": "52.5237580",
-     "longitude": "-2.0492960"
-   }
- },
- {
-   "name": "Stafford Rail Station",
-   "nlc": "800",
-   "isArrived": false,
-   "coordinates": {
-     "latitude": "52.8039050",
-     "longitude": "-2.1220170"
-   }
- },
- {
-   "name": " Stoke-on-Trent Rail Station",
-   "nlc": "900",
-   "isArrived": false,
-   "coordinates": {
-     "latitude": "53.0869600",
-     "longitude": "-2.2432950"
-   }
- },
- {
-   "name": "Macclesfield Rail Station",
-   "nlc": "1000",
-   "isArrived": false,
-   "coordinates": {
-     "latitude": "52.2593310",
-     "longitude": "-2.1222040"
-   }
- },
- {
-   "name": "Stockport Rail Station",
-   "nlc": "1100",
-   "isArrived": false,
-   "coordinates": {
-     "latitude": "53.4058860",
-     "longitude": "-2.1629980"
-   }
- },
- {
-   "name": "Manchester Piccadilly Rail Station",
-   "nlc": "1200",
-   "isArrived": false,
-   "coordinates": {
-     "latitude": "53.4777790",
-     "longitude": "-2.2319190"
+     "latitude": "12.928716",
+     "longitude": "77.6289"
    }
  }]
 }'
